34eae50a99b542a480635dc68079a94697d06b69
TodorGaby <gabriel.todor04@gmail.com>
2024-11-29 20:04:45 +0200

    final

---
+++ src/main/java/Lab8_JDBC/ExceptieAnExcursie.java
@@ -0,0 +1,7 @@
+package Lab8_JDBC;
+
+public class ExceptieAnExcursie extends Exception {
+    public ExceptieAnExcursie(String mesaj) {
+        super(mesaj);
+    }
+}
---
+++ src/main/java/Lab8_JDBC/ExceptieVarsta.java
@@ -0,0 +1,8 @@
+package Lab8_JDBC;
+
+public class ExceptieVarsta extends Exception {
+    public ExceptieVarsta(String mesaj) {
+        super(mesaj);
+    }
+}
+
